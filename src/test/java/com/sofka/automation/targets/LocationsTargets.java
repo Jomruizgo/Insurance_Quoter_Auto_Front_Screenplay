138c422a6b972e26317cf77aefd4ceab7cd25860
Jomruizgo <jomruizgo@gmail.com>
2026-04-24 06:22:50 -0500

    feat: add LocationsTargets with badge/banner/next-button locators [#21]

---
+++ src/test/java/com/sofka/automation/targets/LocationsTargets.java
@@ -0,0 +1,23 @@
+package com.sofka.automation.targets;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public final class LocationsTargets {
+
+    public static Target badgeForIndex(int index) {
+        return Target.the("badge for location " + index)
+            .locatedBy("//table[contains(@class,'locations-table')]"
+                + "//tbody/tr[td[2][normalize-space(text())='" + index + "']]"
+                + "//span[contains(@class,'badge')]");
+    }
+
+    public static final Target BLOCKING_ALERTS_BANNER = Target.the("blocking alerts banner")
+        .located(By.cssSelector("div.alert-banner.alert-banner--warn"));
+
+    public static final Target NEXT_BUTTON = Target.the("siguiente button")
+        .locatedBy("//div[contains(@class,'page-nav')]"
+            + "//button[contains(normalize-space(.),'Siguiente')]");
+
+    private LocationsTargets() {}
+}
