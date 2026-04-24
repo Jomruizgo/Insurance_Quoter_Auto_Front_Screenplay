c9a015a4cfee5a9bcd0bb5d56caf55f54ea86519
Jomruizgo <jomruizgo@gmail.com>
2026-04-24 06:23:28 -0500

    feat: add LocationScenarioSetupHook @Before(order=2) [#23]

---
+++ src/test/java/com/sofka/automation/hooks/LocationScenarioSetupHook.java
@@ -0,0 +1,14 @@
+package com.sofka.automation.hooks;
+
+import com.sofka.automation.tasks.api.SetupLocationScenario;
+import io.cucumber.java.Before;
+import net.serenitybdd.screenplay.actors.OnStage;
+
+public class LocationScenarioSetupHook {
+
+    @Before(order = 2, value = "@location-registration")
+    public void setupLocationScenario() {
+        OnStage.theActorCalled("agent")
+            .attemptsTo(SetupLocationScenario.forCurrentActor());
+    }
+}
