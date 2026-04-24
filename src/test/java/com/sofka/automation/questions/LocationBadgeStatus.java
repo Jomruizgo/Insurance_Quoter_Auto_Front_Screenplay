9d3c8d58a31574a937a064965ef0b393c85b3d1f
Jomruizgo <jomruizgo@gmail.com>
2026-04-24 06:23:31 -0500

    feat: add LocationBadgeStatus Question<String> for badge text by index [#24]

---
+++ src/test/java/com/sofka/automation/questions/LocationBadgeStatus.java
@@ -0,0 +1,39 @@
+package com.sofka.automation.questions;
+
+import com.sofka.automation.targets.LocationsTargets;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.annotations.Subject;
+import net.serenitybdd.screenplay.questions.WebElementQuestion;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+import java.util.List;
+
+@Subject("the badge text for location #?index")
+public class LocationBadgeStatus implements Question<String> {
+
+    private final int index;
+
+    private LocationBadgeStatus(int index) {
+        this.index = index;
+    }
+
+    public static LocationBadgeStatus forLocationIndex(int index) {
+        return new LocationBadgeStatus(index);
+    }
+
+    @Override
+    public String answeredBy(Actor actor) {
+        try {
+            Target badge = LocationsTargets.badgeForIndex(index);
+            List<WebElementFacade> elements = badge.resolveAllFor(actor);
+            if (elements.isEmpty()) {
+                return "";
+            }
+            return elements.get(0).getText().trim();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
